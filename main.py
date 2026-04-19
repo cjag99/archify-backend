77b5e6175cca0917d78a4fb3a8a6552dea7ffecd
cjag99 <cjag99@gmail.com>
2026-04-19 10:59:07 +0200

    add templates for admin login

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from utils.renderer import render
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/test_render", response_class=HTMLResponse)
 @render("index.html")
