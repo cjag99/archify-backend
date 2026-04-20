dbf726cb1ac6f33464243898b6c4d6131229e33b
cjag99 <cjag99@gmail.com>
2026-04-20 22:07:33 +0200

    update dependencies and add supabase client

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from utils.renderer import render
+from supabase import Client
+
+load_dotenv()
+
+db_params = {
+    "url": os.getenv("SUPABASE_URL"),
+    "key": os.getenv("SUPABASE_KEY")
+}
+if db_params.get("url") and db_params.get("key"):
+    supabase = Client(db_params["url"], db_params["key"])
 
 app = FastAPI()
 
