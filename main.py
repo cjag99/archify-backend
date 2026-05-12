513673194675cd3e2fbcbe28c9bc515f0344206e
cjag99 <cjag99@gmail.com>
2026-05-12 16:10:38 +0200

    fix: added max age to middleware to remember origin & credentials

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    max_age=600,
 )
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
