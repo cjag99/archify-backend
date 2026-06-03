d1f4220b39736076e56336b23df21b4a26d64f77
cjag99 <cjag99@gmail.com>
2026-06-03 17:27:07 +0200

    feat: add logout route

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
-from app.api.dependencies import get_auth_service
+
+from app.api.dependencies import get_auth_service, get_current_user
 from app.domain.auth.dtos import UserLoginRequest, UserRegistrationRequest
 from app.domain.auth.services import AuthService
 from app.domain.users.models import UserProfile
@@ -37,5 +38,22 @@ async def login_user(
     try:
        response = service.login_user(data)
        return response
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.post("/logout", response_model=dict)
+async def logout_user(
+    service: AuthService = Depends(get_auth_service),
+    user_auth: tuple[UserProfile, str] = Depends(get_current_user)
+) -> dict:
+    """
+    Log out the current user.
+
+    Inactivates or blacklists the user's current session/token.
+    """
+    try:
+        token = user_auth[1]
+        service.logout_user(token)
+        return {"detail": "Successfully logged out"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
